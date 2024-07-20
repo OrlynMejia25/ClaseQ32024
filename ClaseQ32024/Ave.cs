095550e874b2e3e75c50d01cdb5d957675d20ba1
OrlynMejia25 <orlyncastro@gmail.com>
2024-07-20 00:18:50 -0600

    Creacion y funcionalidad de la clase Ave

---
+++ ClaseQ32024/Ave.cs
@@ -0,0 +1,45 @@
+﻿
+
+namespace ClaseQ32024
+{
+    public class Ave
+    {
+        public string Nombre { get; set; }
+
+        public string Color { get; set; }
+
+        public string Tamano { get; set; }
+
+        public string Familia { get; set; }
+
+        public string sexoBiologico { get; set; }
+
+        public bool EsVoladora { get; set; }
+
+        // constructor
+        public Ave()
+        {
+            EsVoladora = true;
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Color: {Color}");
+            Console.WriteLine($"Tamaño: {Tamano}");
+            Console.WriteLine($"Familia: {Familia}");
+            Console.WriteLine($"Sexo Biologico: {sexoBiologico}");
+          //  Console.WriteLine($"Es Voladora: {(EsVoladora ? "si" : "No")}");
+            Console.WriteLine();
+
+            if (EsVoladora)
+            {
+                Console.WriteLine($"Es Voladora: si");
+            }
+            else
+            {
+                Console.WriteLine($"Es Voladora: no");
+            }
+        }
+    }
+}
--- ClaseQ32024/Program.cs
+++ ClaseQ32024/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using ClaseQ32024;
+// Instanciar la clase o declarar un nuevo objeto
+Ave ave = new Ave();
+ave.Nombre = "Pajaro";
+ave.Color = "Varios";
+ave.Tamano = "Pequeño";
+ave.Familia = "Familia de pajaros";
+ave.sexoBiologico = "Macho";
+ave.Imprimir();
+
+Ave ave2 = new Ave();
+ave2.Nombre = "Pajaro carpintero";
+ave2.Color = "Rojo";
+ave2.Tamano = "Mediano";
+ave2.Familia = "Familia de pajaros carpinteros";
+ave2.sexoBiologico = "Hembra";
+ave2.EsVoladora = false;
+ave2.Imprimir();
+
